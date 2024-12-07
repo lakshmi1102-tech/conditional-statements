fae4dcee1c371b0c96d06b2bb886359eef37c0b7
lakshmi1102-tech <konankilakshmi0211@gmail.com>
2024-12-07 15:36:51 +0530

    conditional statements

---
+++ dat2js.js
@@ -0,0 +1,33 @@
+
+// for vote//
+ var userinput=prompt("enter your age");
+  if(age>=18){
+    console.log("eligible to vote");
+   }
+   else{
+  console.log("not eligible to vote");
+   }
+//   for marriage//
+var age = 20;
+var gender = "male";
+if((age >= 21&& gender==="male")||(age >= 18&& gender==="female")){
+    console.log("eligible for marriage");
+}else{
+    console.log("not eligible for marriage")
+}
+
+
+//driving licence//
+var attendence = 80;
+if(attendence >= 75){
+    console.log("eligible to write exam");
+}else{
+    console.log("not eligible to write exam");
+}
+//senior citizenship//
+ var age = 55;
+ if(age >= 60){
+    console.log("qualify for senior citizenship ")
+ }else{
+    console.log("not qualify for senior citizenship")
+ }
